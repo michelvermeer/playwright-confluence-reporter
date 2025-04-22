cae7965d8e297a47d963a0ce5d59294e9ce67e3a
Michel Vermeer <mvermeer85@gmail.com>
2025-04-22 13:39:59 +0200

    V0.2.0 -- Error and meta info

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A Playwright reporter to post test results on Confluence.
 
+![Example](https://github.com/michelvermeer/playwright-confluence-reporter/blob/main/example.png?raw=true)
+
 ## Installation
 
 ```
@@ -22,8 +24,29 @@ reporter: [
         apiKey: "{Atlassian API key}",
         spaceId: "{The Space ID for the page}",
         parentPageId: "{Optional - The parent page ID}",
+        pageTitle: "E2E test results",
+        generatePage: true,
+        meta: [
+          {
+            key: "Scope",
+            value: "Web, Mobile"
+          }
+        ]
       },
     ],
     ... // Other reporters
   ],
 ```
+
+Options
+
+| Option        | Type                    | Required | Default | Description                                                     |
+| ------------- | ----------------------- | -------- | ------- | --------------------------------------------------------------- |
+| confluenceUrl | _string_                | yes      |         | Your Confluence URL, probably ending with `.atlassian.net/wiki` |
+| username      | _string_                | yes      |         | Your Confluence username / e-mail                               |
+| apiKey        | _string_                | yes      |         | API key obtained from Atlassian                                 |
+| spaceId       | _string_                | yes      |         | The ID of the Space where the page will be created              |
+| parentPageId  | _string_                | no       |         | The parent page ID                                              |
+| pageTitle     | _string_                | no       | date    | Custom page title for the test results                          |
+| generatePage  | _boolean_               | no       | true    | Set to false if you don't want to create a page                 |
+| meta          | _Array[{ key, value }]_ | no       |         | Custom information send with the test results                   |
--- src/confluence-reporter.ts
+++ src/confluence-reporter.ts
@@ -1,158 +1,176 @@
 import type {
-  FullResult,
-  Reporter,
-  TestCase,
-  TestResult,
-} from "@playwright/test/reporter";
-
-interface ConfluenceConfig {
-  confluenceUrl: string;
-  username: string;
-  apiKey: string;
-  spaceId: string;
-  parentPageId?: string;
-}
-
-interface TestSummary {
-  status: string;
-  startTime: Date | string;
-  duration: number;
-}
-
-class ConfluenceReporter implements Reporter {
-  private results: any;
-  private confluenceConfig: ConfluenceConfig;
-  private testSummary: TestSummary | undefined;
-
-  constructor(options: ConfluenceConfig) {
-    const { confluenceUrl, username, apiKey, spaceId, parentPageId } = options;
-    this.confluenceConfig = {
-      confluenceUrl,
-      username,
-      apiKey,
-      spaceId,
-      parentPageId,
-    };
-
-    this.results = {};
+    FullResult,
+    Reporter,
+    TestCase,
+    TestResult,
+  } from "@playwright/test/reporter";
+  
+  interface ConfluenceConfig {
+    confluenceUrl: string;
+    username: string;
+    apiKey: string;
+    spaceId: string;
+    parentPageId?: string;
+    generatePage?: boolean;
+    pageTitle?: string;
+    meta?: Array<{ key: string; value: string }>;
   }
-
-  onTestEnd(test: TestCase & { _projectId?: string }, result: TestResult) {
-    const suiteName = test.parent.title;
-    const testTitle = test.title;
-    const projectName = test._projectId || "unknown";
-    const status = result.status;
-    const duration = result.duration;
-    const steps = result.steps
-      .filter((step) => step.category === "test.step")
-      .map((step) => step.title);
-    const description =
-      steps.length > 0 ? `<ul><li>${steps.join("</li><li>")}</li></ul>` : "";
-
-    if (!this.results[suiteName]) this.results[suiteName] = {};
-    if (!this.results[suiteName][testTitle])
-      this.results[suiteName][testTitle] = {};
-
-    this.results[suiteName][testTitle][projectName] = {
-      status,
-      duration,
-      description,
-    };
+  
+  interface TestSummary {
+    status: string;
+    startTime: Date | string;
+    duration: number;
   }
-
-  async onEnd(result: FullResult) {
-    if (!this.confluenceConfig) {
-      console.log("❌ Confluence config not found");
-      return;
+  
+  class ConfluenceReporter implements Reporter {
+    private results: any;
+    private confluenceConfig: ConfluenceConfig;
+    private testSummary: TestSummary | undefined;
+  
+    constructor(options: ConfluenceConfig) {
+      this.confluenceConfig = {
+        generatePage: true,
+        ...options
+      };
+  
+      this.results = {};
     }
-
-    this.testSummary = result;
-
-    const date = new Date().toISOString().split("T");
-    const time = date[1].split(":");
-    const pageTitle = date[0] + " " + time[0] + ":" + time[1];
-    const pageBody = this.generateConfluenceTable();
-
-    if (!pageBody) {
-      console.error("❌ Failed to generate test table");
-      return;
+  
+    onTestEnd(test: TestCase & { _projectId?: string }, result: TestResult) {
+      const suiteName = test.parent.title;
+      const testTitle = test.title;
+      const projectName = test._projectId || "unknown";
+      const status = result.status;
+      const duration = result.duration;
+      const error = result.error?.message || "";
+      const steps = result.steps
+        .filter((step) => step.category === "test.step")
+        .map((step) => step.title);
+      const description =
+        steps.length > 0 ? `<h6>${steps.join("</h6><h6>")}</h6>` : "";
+  
+      if (!this.results[suiteName]) this.results[suiteName] = {};
+      if (!this.results[suiteName][testTitle])
+        this.results[suiteName][testTitle] = {};
+  
+      this.results[suiteName][testTitle][projectName] = {
+        status,
+        duration,
+        description,
+        error,
+      };
     }
+  
+    async onEnd(result: FullResult) {
+      const { confluenceUrl, username, apiKey, spaceId, generatePage, pageTitle } = this.confluenceConfig;
+  
+      if (!confluenceUrl || !username || !apiKey || !spaceId) {
+        console.error("❌ Missing Confluence configuration", this.confluenceConfig);
+        return;
+      }
+      
+      this.testSummary = result;
+  
+      if (!generatePage) {
+        console.info("Skipping Confluence page generation");
+        return;
+      }
 
-    await this.createConfluencePage(pageTitle, pageBody);
-  }
-
-  generateConfluenceTable() {
-    if (!this.testSummary) {
-      console.error("❌ Test summary not found");
-      return;
+      const confluencePageTitle = pageTitle || new Date().toLocaleString('en-US', { dateStyle: 'short', timeStyle: 'short' });
+      const pageBody = this.generateConfluenceTable();
+  
+      if (!pageBody) {
+        console.error("❌ Failed to generate test table");
+        return;
+      }
+  
+      await this.createConfluencePage(confluencePageTitle, pageBody);
     }
+  
+    generateConfluenceTable() {
+      if (!this.testSummary) {
+        return;
+      }
+  
+      let html = `<p><strong>Status:</strong> ${this.testSummary.status}</p><p><strong>Duration:</strong> ${(this.testSummary.duration / 1000).toFixed(1)} sec.</p>`;
 
-    let html = `<p>Status: ${
-      this.testSummary.status
-    }</p><p>Duration (ms): ${Math.round(
-      this.testSummary.duration
-    )}</p><table><thead><tr><th>Suite</th><th>Test</th><th>Project</th><th>Status</th><th>Steps</th></tr></thead><tbody>`;
+      if (this.confluenceConfig.meta) {
+          html += this.confluenceConfig.meta.map((item) => `<p><strong>${item.key}:</strong> ${item.value}</p>`).join('');
+      }
 
-    for (const suite in this.results) {
-      for (const test in this.results[suite]) {
-        for (const project in this.results[suite][test]) {
-          const { status, description } = this.results[suite][test][project];
-          html += `
-                <tr>
-                    <td>${suite}</td>
-                    <td>${test}</td>
-                    <td>${project}</td>
-                    <td>${status === "passed" ? "✅" : "❌"}</td>
-                    <td>${description}</td>
-                </tr>
-            `;
+      html += `<table><thead><tr><th>Suite</th><th>Test</th><th>Project</th><th>Status</th><th>Duration</th><th>Steps</th></tr></thead><tbody>`;
+  
+      for (const suite in this.results) {
+        for (const test in this.results[suite]) {
+          for (const project in this.results[suite][test]) {
+            const { status, description, duration, error } = this.results[suite][test][project];
+            html += `
+                  <tr>
+                      <td><h6>${suite}</h6></td>
+                      <td><h6>${test}</h6></td>
+                      <td><h6>${project}</h6></td>
+                      <td><h6>${status === "passed" ? "✅" : "❌"}</td>
+                      <td><h6>${(duration / 1000).toFixed(1)} sec.</td>
+                      <td>${description}</td>
+                  </tr>
+              `;
+            
+            if (error) {
+              html += `
+                  <tr>
+                      <td colspan="6">
+                          <h6 style="color: red;">${error}</h6>
+                      </td>
+                  </tr>
+              `;
+            }
+          }
         }
       }
+  
+      html += `</tbody></table>`;
+      return html;
     }
-
-    html += `</tbody></table>`;
-    return html;
-  }
-
-  async createConfluencePage(title: string, body: string) {
-    const { confluenceUrl, username, apiKey, spaceId, parentPageId } =
-      this.confluenceConfig;
-
-    if (!confluenceUrl || !username || !apiKey || !spaceId) {
-      console.error("❌ Missing Confluence configuration");
-      return;
-    }
-
-    const auth = Buffer.from(`${username}:${apiKey}`).toString("base64");
-
-    const bodyData = {
-      spaceId,
-      parentId: parentPageId,
-      status: "current",
-      title,
-      body: {
-        representation: "storage",
-        value: body,
-      },
-    };
-
-    try {
-      const request = await fetch(`${confluenceUrl}/api/v2/pages`, {
-        method: "POST",
-        headers: {
-          Authorization: `Basic ${auth}`,
-          Accept: "application/json",
-          "Content-Type": "application/json",
+  
+    async createConfluencePage(title: string, body: string) {
+      const { confluenceUrl, username, apiKey, spaceId, parentPageId } =
+        this.confluenceConfig;
+  
+      if (!confluenceUrl || !username || !apiKey || !spaceId) {
+        console.error("❌ Missing Confluence configuration", this.confluenceConfig);
+        return;
+      }
+  
+      const auth = Buffer.from(`${username}:${apiKey}`).toString("base64");
+  
+      const bodyData = {
+        spaceId,
+        parentId: parentPageId,
+        status: "current",
+        title,
+        body: {
+          representation: "storage",
+          value: body,
         },
-        body: JSON.stringify(bodyData),
-      });
-      const res = await request.json();
-      const url = `${res._links.base}${res._links.webui}`;
-      console.log("✅ Confluence page created", url);
-    } catch (error) {
-      console.error("❌ Failed to create Confluence page:", error);
+      };
+  
+      try {
+        const request = await fetch(`${confluenceUrl}/api/v2/pages`, {
+          method: "POST",
+          headers: {
+            Authorization: `Basic ${auth}`,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(bodyData),
+        });
+        const res = await request.json();
+        const url = `${res._links.base}${res._links.webui}`;
+        console.log("✅ Confluence page created", url);
+      } catch (error) {
+        console.error("❌ Failed to create Confluence page:", error);
+      }
     }
   }
-}
-
-export default ConfluenceReporter;
+  
+  export default ConfluenceReporter;
